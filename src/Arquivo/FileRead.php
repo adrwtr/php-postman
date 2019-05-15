8704889114f77bbfa786ae05acf4189190ac2851
Adriano Waltrick <adriano@unimestre.com>
2019-05-15 09:11:23 -0300

    Leitura do arquivo

--- src/Arquivo/FileRead.php
+++ src/Arquivo/FileRead.php
@@ -1,55 +1,20 @@
 <?php
-namespace PhpFlo\Component\Arquivo;
+namespace Phppostman\Arquivo;
 
-use PhpFlo\Common\ComponentInterface;
-use PhpFlo\Common\ComponentTrait;
-
-class FileRead implements ComponentInterface
+class FileRead
 {
-    use ComponentTrait;
-
     public function __construct()
     {
-        $this->inPorts()
-            ->add(
-                'ds_file_path',
-                ['datatype' => 'string']
-            );
 
-        $this->outPorts()
-            ->add(
-                'out',
-                ['datatype' => 'string']
-            )
-            ->add(
-                'error',
-                []
-            );
-
-        $this->inPorts()
-            ->ds_file_path
-            ->on(
-                'data',
-                [$this, 'lerArquivo']
-            );
     }
 
 
-    public function lerArquivo($data)
+    public static function lerArquivo($data)
     {
         if (!file_exists($data)) {
-            $this->outPorts()
-                ->error
-                ->send(
-                    "File {$data} doesn't exist"
-                );
-
-            return;
+            return null;
         }
 
-        $this->outPorts()
-            ->out
-            ->send(file_get_contents($data))
-            ->disconnect();
+        return file_get_contents($data);
     }
 }
