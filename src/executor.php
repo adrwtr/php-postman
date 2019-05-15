c54715f9db939dec3ab704409d7410b6e1b56469
Adriano Waltrick <adriano@unimestre.com>
2019-05-15 09:35:35 -0300

    WIP - executando a API

--- src/executor.php
+++ src/executor.php
@@ -1,1 +1,14 @@
 <?php
+require '../vendor/autoload.php';
+
+use Phppostman\Arquivo\FileRead;
+
+$ds_api = $_REQUEST['api'];
+$ds_path = '../api/' . $ds_api;
+
+dump($ds_path);
+
+$ds_content = FileRead::lerArquivo($ds_path);
+
+dump($ds_content);
+
