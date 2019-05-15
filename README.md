01b45e1b02ae3f558d84b71b4d9f4410cb72a2e5
Adriano Waltrick <adriano@unimestre.com>
2019-05-15 09:41:14 -0300

    Execução completa

--- README.md
+++ README.md
@@ -2,8 +2,10 @@
 
 Executor de APIs em PHP
 
-
-
-
 # php libs
 GUZZLE
+
+
+# APIS
+
+http://localhost/api/executor.php?api=gateway-unimestre/recupera-vid
--- src/executor.php
+++ src/executor.php
@@ -2,13 +2,30 @@
 require '../vendor/autoload.php';
 
 use Phppostman\Arquivo\FileRead;
+use Phppostman\Conversor\Postman;
+use Phppostman\Http\Guzzle;
+
+$objPostman = new Postman();
+$objGuzzle = new Guzzle();
 
 $ds_api = $_REQUEST['api'];
 $ds_path = '../api/' . $ds_api;
 
-dump($ds_path);
-
 $ds_content = FileRead::lerArquivo($ds_path);
 
-dump($ds_content);
+$arrDados = $objPostman->converter($ds_content);
 
+$objGuzzle->setUrl(
+    $arrDados['ds_url']
+)->setArrHeaders(
+    $arrDados['arrHeaders']
+)->setJson(
+    $arrDados['ds_json']
+);
+
+$resultado = $objGuzzle->executar(
+    $arrDados['ds_metodo']
+);
+
+
+echo $resultado;
